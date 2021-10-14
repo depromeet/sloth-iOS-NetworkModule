f876006a17a6837038195b80947d2641abbff2fe
1Consumption <hanseop95@gmail.com>
2021-10-14 21:33:53 +0900

    HTTPMethod 추가

---
+++ SlothNetworkModule/Sources/SlothNetworkModule/HTTPMethod.swift
@@ -0,0 +1,23 @@
+//
+//  HTTPMethod.swift
+//  
+//
+//  Created by Olaf on 2021/10/14.
+//
+
+import Foundation
+
+public enum HTTPMethod: CustomStringConvertible {
+    
+    case get
+    case post
+    
+    public var description: String {
+        switch self {
+        case .get:
+            return "GET"
+        case .post:
+            return "POST"
+        }
+    }
+}
